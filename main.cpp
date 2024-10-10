0242b7f6161894fc56d6efefc5d0459f32482bcc
Bernt Viggo Matheussen <163319619+berntmath@users.noreply.github.com>
2024-10-10 14:18:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
 // Testfile 
 // BVM, 10 nov 2024
-//
+
+#include <stdio.h>
+#include <sys/time.h>
+
+//////////////////////////////////////////////////////
+int main(int argc, char *argv[]) {
+
+  printf("Hello WOrld\n");
+  
+    return 0;
+}
+
 
